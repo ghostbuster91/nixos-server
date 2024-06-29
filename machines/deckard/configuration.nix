a586b612ca4bdb44fbf88b9a35c87369bbbf148b
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2024-06-29 09:05:35 +0200

    Add lshw

--- machines/deckard/configuration.nix
+++ machines/deckard/configuration.nix
@@ -132,6 +132,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     wget
+    lshw
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
